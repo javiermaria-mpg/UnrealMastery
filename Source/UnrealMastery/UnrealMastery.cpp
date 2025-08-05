375e8cec41cce62f27da02df7a0314d166c83246
javiermaria <javiermaria@themultiplayerguys.com>
2025-08-05 09:22:20 +0200

    Add default Unreal source and config files

---
+++ Source/UnrealMastery/UnrealMastery.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealMastery.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealMastery, "UnrealMastery" );
